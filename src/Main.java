5f811eff963f441fed4494ecf3c0b8cb439700c4
xbvpx <butek@inbox.ru>
2023-03-02 22:44:51 +0300

    lambda

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> person = new ArrayList<>();
-        PersonSurnameLengthComparator personComporator = new PersonSurnameLengthComparator();
         person.add(new Person("Иванов", "Иван", 17));
         person.add(new Person("Игнатьев", "Игорь", 23));
         person.add(new Person("Потрясов", "Антон", 38));
         person.add(new Person("Ануфреев", "Олег", 13));
-        Collections.sort(person, personComporator);
 
+        Comparator<Person> comparator = (o1, o2) -> {
+            int lengt1 = o1.getSurname().split("\\P{IsAlphabetic}+").length;
+            int lengt2 = o2.getSurname().split("\\P{IsAlphabetic}+").length;
+            if (lengt1 != lengt2) {
+                return Integer.compare(lengt2, lengt1);
+            }
+            return Integer.compare(o1.getAge(), o2.getAge());
+        };
+
+        Collections.sort(person, comparator);
         System.out.println(person);
     }
 }
